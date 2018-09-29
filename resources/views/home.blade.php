ba65ddc8af9f336798c0399641dda17724f8bff4
Jesse Horne <j.horne2796@gmail.com>
2018-09-29 10:55:57 -0500

    updated form style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,10 +33,10 @@
 				<form action="https://github.us8.list-manage.com/subscribe/post?u=4fc9b6556afcf6ca527c3b4fe&amp;id=01d30a02ef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 						<div id="mc_embed_signup_scroll">
 
-						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+						<input type="email" value="" name="EMAIL" class="tw-email" id="mce-EMAIL" placeholder="email address" required>
 				    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_4fc9b6556afcf6ca527c3b4fe_01d30a02ef" tabindex="-1" value=""></div>
-				    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="tw-button"></div>
 				    </div>
 				</form>
 				</div>
